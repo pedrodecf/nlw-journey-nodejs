1a3d0e7acb9914384005302150df50784e53bd7c
pedrodecf <pedro012014@gmail.com>
2024-07-11 19:51:49 -0300

    feat: Add routes for creating and retrieving links

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { confirmTrip } from './routes/confirm-trip'
 import { createTrip } from './routes/create-trip'
 import { createActivity } from './routes/create-activity'
 import { getActivities } from './routes/get-activities'
+import { createLink } from './routes/create-link'
+import { getLinks } from './routes/get-links'
 
 const app = fastify()
 
@@ -24,6 +26,8 @@ app.register(confirmTrip)
 app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
   console.log('Server running on port 3000 🚀')
