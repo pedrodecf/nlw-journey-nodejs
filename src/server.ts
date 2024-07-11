8d8d27eb1fe3136ac0c15ef39906ca9b6aa0a656
pedrodecf <pedro012014@gmail.com>
2024-07-11 19:46:28 -0300

    feat: Add migration and models for activities and links tables and add create and get activity functionality

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {
 import { confirmParticipant } from './routes/confirm-participant'
 import { confirmTrip } from './routes/confirm-trip'
 import { createTrip } from './routes/create-trip'
+import { createActivity } from './routes/create-activity'
+import { getActivities } from './routes/get-activities'
 
 const app = fastify()
 
@@ -20,6 +22,8 @@ app.register(cors, {
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipant)
+app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
   console.log('Server running on port 3000 🚀')
