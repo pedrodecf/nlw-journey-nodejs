7dea9178723e7d444d6e75e4104e287d0588cd87
pedrodecf <pedro012014@gmail.com>
2024-07-11 18:10:35 -0300

    feat: Add confirm trip route

---
+++ src/routes/confirm-trip.ts
@@ -0,0 +1,90 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { dayjs } from '../lib/dayjs'
+import { getMailCient } from '../lib/mail'
+import nodemailer from 'nodemailer'
+
+export async function confirmTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/trips/:tripId/confirm',
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+
+    async (request, reply) => {
+      const { tripId } = request.params
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+        include: {
+          participants: {
+            where: {
+              is_owner: false,
+            },
+          },
+        },
+      })
+
+      if (!trip) {
+        throw new Error('Trip not found')
+      }
+
+      if (trip.is_confirmed) {
+        return reply.redirect(`http://localhost:3000/trips/${tripId}`)
+      }
+
+      await prisma.trip.update({
+        where: {
+          id: tripId,
+        },
+        data: {
+          is_confirmed: true,
+        },
+      })
+
+      const formattedStartDate = dayjs(trip.starts_at).format('LL')
+      const formattedEndDate = dayjs(trip.ends_at).format('LL')
+
+      const mail = await getMailCient()
+
+      await Promise.all(
+        trip.participants.map(async (participant) => {
+          const confirmationLink = `http://localhost:3000/trips/${trip.id}/confirm/${participant.id}`
+          const message = await mail.sendMail({
+            from: {
+              name: 'Trip Planner',
+              address: 'contato@planner.com.br',
+            },
+            to: participant.email,
+            subject: `Confirme sua viagem para ${trip.destination} em ${formattedStartDate} ✈️🧳`,
+            html: `
+            <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+            <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}, </strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+            </br>
+            <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+            </br>
+            <p>
+              <a href="${confirmationLink}">Confirmar viagem</a>
+            </p>
+            </br>
+            <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+            </div>
+            `.trim(),
+          })
+
+          console.log(nodemailer.getTestMessageUrl(message))
+        }),
+      )
+
+      return reply.redirect(`http://localhost:3000/trips/${tripId}`)
+    },
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,20 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { confirmTrip } from './routes/confirm-trip'
+import cors from '@fastify/cors'
 
 const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.register(cors, {
+  origin: '*',
+})
+
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
   console.log('Server running on port 3000 🚀')
